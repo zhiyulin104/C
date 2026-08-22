c9dc13e126269b2efd312d0e5e93251419c6a24d
zhiyulin104 <zhiyulin104@gmail.com>
2026-08-22 15:46:24 +0800

    Update and rename 大小 to 大小.cpp

--- C++ 光點創意/大小.cpp
+++ C++ 光點創意/大小.cpp
@@ -1,4 +1,15 @@
-int a, b, c;
+#include <iostream>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+//o kjs;dlkj;flgk s;lfkg ;gjb rtkg;lfjgl;kt;kojlj;sgkldfj;gsjdl;fkjg;lskdjf;lgkj;sldkfjg;lkjs;lgkjf;kljh
+
+
+
+
+using namespace std;
+int main(int argc, char** argv) {
+	int a, b, c;
 	cout << "請分別輸入abc"; 
 	cin >> a >> b >> c;
 	if (a > b) {
